2fbe2c5c79c3417a30bf591188b6e58a83ffb3cc
coderz365 <hanssea@163.com>
2021-07-26 16:20:12 +0800

    feat(element-plus): increment element-plus

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,5 +1,11 @@
 <template>
   <h2>home page + {{ $store.state.name }}</h2>
+  <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
 </template>
 
 <script lang="ts">
